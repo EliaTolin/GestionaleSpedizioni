d8d267ed0d52346588e5ca1b9ad70b3cf9fb87f5
Elia Tolin <eliatolin@gmail.com>
2021-09-09 13:12:45 +0200

    dev

--- src/main/java/com/eliatolin/gest_spedizioni/models/SpedizioneAssicurata.java
+++ src/main/java/com/eliatolin/gest_spedizioni/models/SpedizioneAssicurata.java
@@ -28,6 +28,12 @@ public class SpedizioneAssicurata extends Spedizione{
     }
     
     @Override
+    public float getValoreAssicurato()
+    {
+        return valoreAssicurato;
+    }
+    
+    @Override
     public void aggiornaStato()
     {
         super.aggiornaStato();
