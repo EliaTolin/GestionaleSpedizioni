ce3bd12efb7e5b5b562a9c2a6bfd5e68b81855d6
Elia Tolin <eliatolin@gmail.com>
2021-09-09 13:25:36 +0200

    dev

--- src/main/java/com/eliatolin/gest_spedizioni/models/Utente.java
+++ src/main/java/com/eliatolin/gest_spedizioni/models/Utente.java
@@ -1,62 +1,62 @@
 package com.eliatolin.gest_spedizioni.models;
+
 import com.eliatolin.gest_spedizioni.utils.DataUtility;
 import com.eliatolin.gest_spedizioni.models.*;
+import java.util.Random;
 
 public class Utente {
-	
-	private final String nomeUtente;
-	private final String password;
-	private final String indirizzo;
-        private int contatore;
-        public String id;
-	
-	public Utente(String nomeUtente, String password, String indirizzo) {
-		
-		this.nomeUtente = nomeUtente;
-		this.password = password;
-		this.indirizzo = indirizzo;
-	}
-        
-	public String getNomeUtente() {
-            return this.nomeUtente;
-	}
-	
-	public String getPassword() {
-            return this.password;
-	}
-        
-        public String getIndirizzo()
-        {
-            return this.indirizzo;
+
+    private final String nomeUtente;
+    private final String password;
+    private final String indirizzo;
+    private int contatore;
+    public String id;
+
+    public Utente(String nomeUtente, String password, String indirizzo) {
+
+        this.nomeUtente = nomeUtente;
+        this.password = password;
+        this.indirizzo = indirizzo;
+    }
+
+    public String getNomeUtente() {
+        return this.nomeUtente;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getIndirizzo() {
+        return this.indirizzo;
+    }
+
+    public ListaSpedizioni getListaSpedizioni() {
+        return DataUtility.getSpedizioniUtente(this.nomeUtente);
+    }
+
+    public int getContatore() {
+        return contatore;
+    }
+
+    public void inc() {
+        contatore++;
+    }
+
+    public void dec() {
+        if (contatore > 0) {
+            contatore--;
         }
-        
-        public ListaSpedizioni getListaSpedizioni()
-        {
-            return DataUtility.getSpedizioniUtente(this.nomeUtente);
-        }
-	
-        public int getContatore() {
-            return contatore;
-        }
-        
-        public void inc() {
-            contatore++;
-        }
-        
-        public void dec() {
-            if(contatore > 0)
-                contatore--;
-        }
-        
-        public void AggiungiSpedizione(Spedizione s) {
-            s.
-        }
-        public String id(String id) {
-            String id = getNomeUtente() + contatore + 
-        }
-        
-	@Override
-	public String toString() {
-		return nomeUtente + ";" + password + ";" + indirizzo;
-	}
+    }
+
+    public String id(String id) {
+        Random rand = new Random();
+        int n = rand.nextInt(1500000);
+        return getNomeUtente() + contatore + n;
+    }
+
+    @Override
+    public String toString() {
+        return nomeUtente + ";" + password + ";" + indirizzo;
+    }
 }
